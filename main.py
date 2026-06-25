7dcacff8d918218dcbd8ad94cd39085dc46f8c02
Dalida Ceren <22051350165@ogr.halic.edu.tr>
2026-06-25 08:23:35 +0300

    Update career goal text

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Dalida"
-goal = "Cloud and DevOps focused Applied AI Developer"
+name = "Dalida Ceren Güney"
+goal = "Cloud & DevOps focused Applied AI Developer"
+duration_months = 18
 
 print("Hello, my name is", name)
 print("My goal is:", goal)
+print("My roadmap duration is :", duration_months)
